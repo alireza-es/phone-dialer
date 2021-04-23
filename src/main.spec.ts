4a6a9b75ef3f9545ce5ac0fba3c305ce0bb39420
Alireza <alireza.es@gmail.com>
2021-04-24 00:07:55 +0430

    Add first test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,3 +1,11 @@
-test('should return true given foo', () => {
-    expect(1).toBe(1)
+import { PhoneKeypad } from './PhoneKeypad';
+test('Dial with n=1', () => {
+    //arrange
+    const keypad = new PhoneKeypad();
+    
+    //act
+    var actual = keypad.generateNumbers(1);
+    
+    //check
+    expect(actual.length).toBe(10);
 })
